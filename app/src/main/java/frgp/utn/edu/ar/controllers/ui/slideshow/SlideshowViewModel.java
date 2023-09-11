6919428a67692cc4cbc5e54634f3b38e3b99aed5
Hernan Varela <hernan.varela3@alumnos.frgp.utn.edu.ar>
2023-09-11 10:00:31 -0300

    Initial commit

---
+++ app/src/main/java/frgp/utn/edu/ar/controllers/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package frgp.utn.edu.ar.controllers.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
