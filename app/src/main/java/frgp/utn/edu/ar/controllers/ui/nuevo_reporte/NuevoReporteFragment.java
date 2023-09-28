bd9212bf331650b6f007db81a50fa1c8d407eb78
Hernan Varela <hernan.varela3@alumnos.frgp.utn.edu.ar>
2023-09-28 15:21:09 -0300

    28-09-2023 | 15:25
    - Se completa fragmento de nuevo reporte.
    - Se dá funcionalidad básica al botón de cámara
    - Se agregan permisos de usos para cámara, almacenamiento y ubicación.

--- app/src/main/java/frgp/utn/edu/ar/controllers/ui/nuevo_reporte/NuevoReporteFragment.java
+++ app/src/main/java/frgp/utn/edu/ar/controllers/ui/nuevo_reporte/NuevoReporteFragment.java
@@ -1,7 +1,12 @@
 package frgp.utn.edu.ar.controllers.ui.nuevo_reporte;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,11 +16,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import frgp.utn.edu.ar.controllers.R;
 
 public class NuevoReporteFragment extends Fragment {
-
+    private static final int CAMERA_PIC_REQUEST = 1337;
     private NuevoReporteViewModel mViewModel;
 
     public static NuevoReporteFragment newInstance() {
@@ -25,7 +31,25 @@ public class NuevoReporteFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_nuevo_reporte, container, false);
+        View view = inflater.inflate(R.layout.fragment_nuevo_reporte, container, false);
+
+        Button bCamara = view.findViewById(R.id.btnCamara);
+        Button bUbicacion = view.findViewById(R.id.btnUbicacion);
+        Button bCrearReporte = view.findViewById(R.id.btnCrearReporte);
+        bCamara.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Verificar si tenemos el permiso de la cámara
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
+                } else {
+                    // Si no tenemos el permiso, solicítalo al usuario
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, CAMERA_PIC_REQUEST);
+                }
+
+            }
+        });
+        return view;
     }
 
     @Override
