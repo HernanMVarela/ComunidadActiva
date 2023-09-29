8cf42a0cd530306ceb2ac33b79e0c97beb214033
Gonzalo Sinnott Segu <gonzalo.sinnott@gmail.com>
2023-09-29 20:43:21 -0300

    typo fix

--- app/src/main/java/frgp/utn/edu/ar/Negocio/ProyectoNegocio.java
+++ app/src/main/java/frgp/utn/edu/ar/Negocio/ProyectoNegocio.java
@@ -5,11 +5,8 @@ import android.content.Context;
 import java.util.List;
 
 import frgp.utn.edu.ar.entidades.EstadoProyecto;
-import frgp.utn.edu.ar.entidades.EstadoUsuario;
 import frgp.utn.edu.ar.entidades.Proyecto;
 import frgp.utn.edu.ar.entidades.TipoProyecto;
-import frgp.utn.edu.ar.entidades.TipoUsuario;
-import frgp.utn.edu.ar.entidades.Usuario;
 
 public interface ProyectoNegocio {
     boolean agregarProyecto(Context context, Proyecto proyecto);
@@ -18,7 +15,7 @@ public interface ProyectoNegocio {
     List<Proyecto> listarProyectos(Context context);
     List<Proyecto> listarProyectosPorOwner(Context context, int idCreador);
     List<Proyecto> listarProyectosPorEstado(Context context, int idEstado);
-    List<Proyecto> listarProyectosPorTipo(Context context, int idTipo);+
+    List<Proyecto> listarProyectosPorTipo(Context context, int idTipo);
     List<EstadoProyecto> listarEstadosProyecto(Context context);
     List<TipoProyecto> listarTiposProyecto(Context context);
 }
