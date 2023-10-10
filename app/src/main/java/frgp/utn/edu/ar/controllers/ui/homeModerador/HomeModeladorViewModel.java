294abadb559825f428896e4c3b900cdbe2431e0e
manuClifton <42977735+manuClifton@users.noreply.github.com>
2023-10-10 15:32:53 -0300

    add moderador

---
+++ app/src/main/java/frgp/utn/edu/ar/controllers/ui/homeModerador/HomeModeladorViewModel.java
@@ -0,0 +1,19 @@
+package frgp.utn.edu.ar.controllers.ui.homeModerador;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeModeladorViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeModeladorViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
