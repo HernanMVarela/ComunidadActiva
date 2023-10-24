15f1828c01922f0e26e25701d4745f16171e6ef0
manuClifton <42977735+manuClifton@users.noreply.github.com>
2023-10-24 01:05:37 -0300

    add listar denuncias
    se modifico la entidad denuncias, se agregaro ListarDenunciaAdapter, y DMAListar denuncia de proyecyo y de reporte ya que son dos tablas separadas.

---
+++ app/src/main/java/frgp/utn/edu/ar/controllers/ui/adapters/ListarDenunciaAdapter.java
@@ -0,0 +1,60 @@
+package frgp.utn.edu.ar.controllers.ui.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+
+import java.util.List;
+
+import frgp.utn.edu.ar.controllers.R;
+import frgp.utn.edu.ar.controllers.data.model.DenunciaNuevo;
+
+public class ListarDenunciaAdapter extends ArrayAdapter<DenunciaNuevo> {
+
+    public ListarDenunciaAdapter(Context context, List<DenunciaNuevo> listaDenuncia) {
+        super(context, R.layout.layout_lista_denuncia, listaDenuncia);
+        setDropDownViewResource(R.layout.layout_lista_denuncia);
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        DenunciaNuevo denuncia = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.layout_lista_denuncia, parent,false);
+        }
+
+        TextView titulo = convertView.findViewById(R.id.tvTituloListaDenuncia);
+        TextView usernamefecha = convertView.findViewById(R.id.tvUsernameFechaListaDenuncia);
+        CardView cardview = convertView.findViewById(R.id.cardview_lista_denuncia);
+        assert denuncia != null;
+
+        titulo.setText(denuncia.getTitulo());
+
+        usernamefecha.setText("Denunciado por " + denuncia.getDenunciante().getUsername() + " el " + denuncia.getFecha_creacion().toString());
+
+        if(denuncia.getEstado().getEstado().equals("CERRADA") || denuncia.getEstado().getEstado().equals("CANCELADA")){
+            cardview.setCardBackgroundColor(ContextCompat.getColor(getContext(), R.color.danger));
+        }
+        if(denuncia.getEstado().getEstado().equals("PENDIENTE")){
+            cardview.setCardBackgroundColor(ContextCompat.getColor(getContext(), R.color.warning));
+        }
+        if(denuncia.getEstado().getEstado().equals("ATENDIDA")){
+            cardview.setCardBackgroundColor(ContextCompat.getColor(getContext(), R.color.green_100));
+        }
+        return convertView;
+    }
+
+
+}
