ca087be081f5715887bd06a1e7d177a3295c51e3
HernanMVarela <hernan.varela3@alumnos.frgp.utn.edu.ar>
2023-11-10 21:12:37 -0300

    10-11-2023 | 21:12
    - Fix botón unirse/abandonar proyecto

--- app/src/main/java/frgp/utn/edu/ar/controllers/data/remote/proyecto/DMABuscarUsuarioEnProyecto.java
+++ app/src/main/java/frgp/utn/edu/ar/controllers/data/remote/proyecto/DMABuscarUsuarioEnProyecto.java
@@ -11,56 +11,43 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import frgp.utn.edu.ar.controllers.data.model.Usuario;
+import frgp.utn.edu.ar.controllers.data.model.Voluntario;
 import frgp.utn.edu.ar.controllers.data.remote.DataDB;
 
-public class DMABuscarUsuarioEnProyecto extends AsyncTask<String, Void, String> {
-    private Context context;
-    private ListView listado;
+public class DMABuscarUsuarioEnProyecto extends AsyncTask<String, Void, Boolean> {
     private static String resultado;
-    private boolean controlNoExiste=true, controlNull=false;
-    private Button botonN;
     private int idUserN, idProyectoN;
-    public DMABuscarUsuarioEnProyecto(int isUserB, int idProyectoB, Button boton, Context ct) {
-        context = ct;
+    public DMABuscarUsuarioEnProyecto(int isUserB, int idProyectoB) {
         idProyectoN = idProyectoB;
         idUserN=isUserB;
-        botonN=boton;
     }
     @Override
-    protected String doInBackground(String... urls) {
+    protected Boolean doInBackground(String... urls) {
         try {
+            Voluntario user = null;
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection(DataDB.urlMySQL, DataDB.user, DataDB.pass);
-            String query = "SELECT ID_USER, ID_PROYECTO, FECHA_SALIDA FROM USERS_PROYECTO WHERE ID_USER = ? AND ID_PROYECTO = ?;";
+            String query = "SELECT ID_USER, ID_PROYECTO, FECHA_SALIDA FROM USERS_PROYECTO WHERE ID_USER = ? AND ID_PROYECTO = ?";
             PreparedStatement preparedStatement = con.prepareStatement(query);
             preparedStatement.setInt(1,idUserN);
             preparedStatement.setInt(2,idProyectoN);
             ResultSet rs = preparedStatement.executeQuery();
-            resultado = " ";
-            while (rs.next()) {
-                controlNoExiste=false;
-                if(rs.getDate("FECHA_SALIDA")==null){
-                    controlNull=true;
+            if(rs.next()){
+                user = new Voluntario();
+                if(rs.getDate("FECHA_SALIDA")!=null){
+                    user.setActivo(false);
+                }else{
+                    user.setActivo(true);
                 }
             }
             rs.close();
             con.close();
-            resultado = "Conexion exitosa";
+            return user.isActivo();
         } catch (Exception e) {
             e.printStackTrace();
-            resultado = "Conexion no exitosa";
+            return false;
         }
-        return resultado;
-    }
-    protected void onPostExecute(String response) {
-        if(controlNoExiste)
-            {botonN.setText("Unirse");}
-        else
-        {
-            if(controlNull)
-            {botonN.setText("Abandonar");}
-            else
-            {botonN.setVisibility(View.GONE);}
-        }
+
     }
 }
